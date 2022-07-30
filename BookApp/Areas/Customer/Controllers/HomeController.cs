b0847ddbb4e909e266b8a6781fddff69de87e10b
Software Jedi <juliandwilliams98@gmail.com>
2022-07-30 11:00:43 -0400

    shoppping cart update, Price update

--- BookApp/Areas/Customer/Controllers/HomeController.cs
+++ BookApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using BookAppSolution.Models;
+﻿using BookAppSolution.DataAccess.Repository.IRepository;
+using BookAppSolution.Models;
+using BookAppSoultion.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,27 @@ namespace BookApp.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            this.unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return View(productList);
+        }
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+            };
+                
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
